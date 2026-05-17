a37e1d50b9da21ad7e5a5443383891dbdb5502b5
SungDae4444 <142758070+SungDae4444@users.noreply.github.com>
2026-05-16 20:39:58 -0700

    Create LibraryStorage.h

---
+++ src/LibraryStorage.h
@@ -0,0 +1,15 @@
+#ifndef LIBRARY_STORAGE
+#define LIBRARY_STORAGE
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Library Storage
+
+class Library Storage
+{
+private:
+  
+public:
+}
