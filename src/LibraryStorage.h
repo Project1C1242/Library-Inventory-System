7d0105e8dd278a26034b78251d4320287ab7011f
pshukla3 <24shuklaparth@gmail.com>
2026-05-17 15:55:27 -0700

    Refactor LibraryStorage header file structure

--- src/LibraryStorage.h
+++ src/LibraryStorage.h
@@ -1,15 +1,41 @@
-#ifndef LIBRARY_STORAGE
-#define LIBRARY_STORAGE
+#ifndef LIBRARY_STORAGE_H
+#define LIBRARY_STORAGE_H
 
 #include <iostream>
 #include <string>
 #include <vector>
+#include <memory>
+#include <stdexcept>
+#include "Item.h"
 
-// Library Storage
+using namespace std;
 
-class Library Storage
-{
+struct Compartment {
+    shared_ptr<Item> item = nullptr;
+};
+
+class Shelf {
 private:
-  
+    vector<Compartment> compartments;
 public:
-}
+    Shelf();
+    Compartment& operator[](size_t index);
+    size_t getSize();
+};
+
+class LibraryStorage {
+private:
+    vector<Shelf> shelves;
+public:
+    LibraryStorage(int numShelves);
+    Shelf& operator[](size_t index);
+
+    void addItem(int shelfIndex, int compIndex, shared_ptr<Item> newItem);
+    void checkoutItem(int shelfIndex, int compIndex, const string& borrower, const string& dueDate);
+    void checkinItem(int shelfIndex, int compIndex);
+    void printItemsInStorage();
+    void printCheckedOutItems();
+    void swapItems(int shelf1, int comp1, int shelf2, int comp2);
+};
+
+#endif
